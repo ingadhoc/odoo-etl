3083a14beb975f959a763c5259247ddfa02977c7
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-05-04 18:45:32 -0300

    IMP

--- addons/product_template_no_create_product/product.py
+++ addons/product_template_no_create_product/product.py
@@ -5,7 +5,6 @@ class product_template(osv.osv):
     _inherit = "product.template"
 
     def create(self, cr, uid, vals, context=None):
-        ''' Store the initial standard price in order to be able to retrieve the cost of a product template for a given date'''
         if not context:
             context = {}
         context['create_product_product'] = True
